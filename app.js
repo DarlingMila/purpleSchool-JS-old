c4d3da5f3b4ba8f579bb0f671ee972e599104468
Ludmila Gubanova <darlingmila@yandex.ru>
2023-08-31 10:35:36 +0300

    add greetings

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("Hello!");
+const language = prompt("Languаge: ru / en / de / it / es / fr ?");
+let greetings;
+
+switch (language) {
+  case "ru":
+    greetings = "Добрый день!";
+    break;
+  case "en":
+    greetings = "Good afternoon!";
+    break;
+  case "de":
+    greetings = "Guten Tag!";
+    break;
+  case "it":
+    greetings = "Buongiorno!";
+    break;
+  case "es":
+    greetings = "¡Buenos días!";
+    break;
+  case "fr":
+    greetings = "Bonjour!";
+    break;
+  default:
+    greetings = "Sul Sul!";
+    break;
+}
+
+console.log(greetings);
