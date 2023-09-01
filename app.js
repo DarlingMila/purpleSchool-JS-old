40091c096ae0dc19856b1dca22d6f722abde51e5
Ludmila Gubanova <darlingmila@yandex.ru>
2023-09-01 16:14:31 +0300

    change to exchange variables

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-const arr = [1, 40, -5, 10, 0];
+const arr = [1, 40, -5, 10, 0, 200, -200];
 
 function sortArray(array) {
+  let count = 0;
   const arrLength = array.length;
 
   for (let i = 0; i < arrLength; i++) {
 
     for (let j = 0; j < arrLength; j++) {
+      count++;
 
       if (array[i] < array[j]) {
-        const num1 = array[i];
-        const num2 = array[j];
+        // const num1 = array[i];
+        // const num2 = array[j];
 
-        array[i] = num2;
-        array[j] = num1;
+        // array[i] = num2;
+        // array[j] = num1;
+
+        [array[i], array[j]] = [array[j], array[i]];
       }
     }
   }
 
+  console.log(count);
   return array;
 }
 
