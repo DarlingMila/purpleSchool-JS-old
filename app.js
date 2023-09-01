fed981d30e617c80dc89c0fe38622baf9855dd34
Ludmila Gubanova <darlingmila@yandex.ru>
2023-09-01 15:03:23 +0300

    change functions to switch

--- app.js
+++ app.js
@@ -1,61 +1,37 @@
-function exchange(money, currencyFrom, currencyTo) {
-  switch (currencyFrom) {
-    case "RUB":
-      return convertRUBTo(money, currencyTo);
+const currencyConverter = (amount, from, to) => {
+  let money;
 
-    case "USD":
-      return convertUSDTo(money, currencyTo);
+  switch (`${from}/${to}`) {
 
-    case "EUR":
-      return convertEURTo(money, currencyTo);
-
+    case "RUB/USD":
+      money = amount * 0.01;
+      break;
+    case "RUB/EUR":
+      money = amount * 0.0096;
+      break;
+    case "USD/RUB":
+      money = amount * 96.02;
+      break;
+    case "USD/EUR":
+      money = amount * 0.92;
+      break;
+    case "EUR/RUB":
+      money = amount * 104.23;
+      break;
+    case "EUR/USD":
+      money = amount * 1.09;
+      break;
     default:
-      return null;
+      money = null;
+      break;
   }
-}
 
-function convertRUBTo(money, currencyTo) {
-  switch (currencyTo) {
-    case "USD":
-      return money * 0.01;
+  return money;
+};
 
-    case "EUR":
-      return money * 0.0096;
-
-    default:
-      return null;
-  }
-}
-
-function convertUSDTo(money, currencyTo) {
-  switch (currencyTo) {
-    case "RUB":
-      return money * 96.02;
-
-    case "EUR":
-      return money * 0.92;
-
-    default:
-      return null;
-  }
-}
-
-function convertEURTo(money, currencyTo) {
-  switch (currencyTo) {
-    case "RUB":
-      return money * 104.23;
-
-    case "USD":
-      return money * 1.09;
-
-    default:
-      return null;
-  }
-}
-
-console.log(exchange(1000, "RUB", "USD"));
-console.log(exchange(500, "USD", "RUB"));
-console.log(exchange(200, "USD", "EUR"));
-console.log(exchange(1700, "EUR", "RUB"));
-console.log(exchange(600, "CNY", "RUB"));
-console.log(exchange(800, "EUR", "CHF"));
+console.log(currencyConverter(1000, "RUB", "USD"));
+console.log(currencyConverter(500, "USD", "RUB"));
+console.log(currencyConverter(200, "USD", "EUR"));
+console.log(currencyConverter(1700, "EUR", "RUB"));
+console.log(currencyConverter(600, "CNY", "RUB"));
+console.log(currencyConverter(800, "EUR", "CHF"));
