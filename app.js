6f8453b2d835177b39b9b6e0080e697d0ec079d4
Ludmila Gubanova <darlingmila@yandex.ru>
2023-09-06 14:42:01 +0300

    reduce number of iterations

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
 const arr = [1, 40, -5, 10, 0, 200, -200];
 
 function sortArray(array) {
-  let count = 0;
+  
+  const copiedArray = [...array];
   const arrLength = array.length;
 
-  for (let i = 0; i < arrLength; i++) {
+  for (let i = 0; i < arrLength - 1; i++) {
 
-    for (let j = 0; j < arrLength; j++) {
-      count++;
-
-      if (array[i] < array[j]) {
-        // const num1 = array[i];
-        // const num2 = array[j];
-
-        // array[i] = num2;
-        // array[j] = num1;
-
-        [array[i], array[j]] = [array[j], array[i]];
+    for (let j = i + 1; j < arrLength; j++) {
+   
+      if (copiedArray[j] < copiedArray[i]) {
+        [copiedArray[i], copiedArray[j]] = [copiedArray[j], copiedArray[i]];
       }
     }
   }
 
-  console.log(count);
-  return array;
+  return copiedArray;
 }
 
 console.log(sortArray(arr));
